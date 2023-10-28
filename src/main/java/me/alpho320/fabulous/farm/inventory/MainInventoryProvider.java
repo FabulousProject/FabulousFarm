60b5e284e3d2645d27e47a42e807b7d5f10eb022
Alpho320 <cahitbaba00@gmail.com>
2023-10-28 22:37:34 +0300

    1.0.0 - init4.

--- src/main/java/me/alpho320/fabulous/farm/inventory/MainInventoryProvider.java
+++ src/main/java/me/alpho320/fabulous/farm/inventory/MainInventoryProvider.java
@@ -8,6 +8,8 @@ import me.alpho320.fabulous.core.util.inv.smartinventory.InventoryProvider;
 import me.alpho320.fabulous.farm.FarmPlugin;
 import me.alpho320.fabulous.farm.gui.Button;
 import me.alpho320.fabulous.farm.gui.GUI;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -23,6 +25,7 @@ public class MainInventoryProvider implements InventoryProvider {
     public MainInventoryProvider(FarmPlugin plugin, GUI gui) {
         this.plugin = plugin;
         this.gui = gui;
+        Particle.BL
     }
 
     @Override
